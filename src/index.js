4aed6de254cc96017cbce6736d71aece4f801272
tarciiz <sidtarcisiosid88@gmail.com>
2023-05-12 09:18:32 -0300

    now goes

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { BrowserRouter, Link } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <BrowserRouter basename={window.location.pathname || ''}>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
